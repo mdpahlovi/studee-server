f6d857d8f30c08920b9c67e48a953e700dbf979f
MD Pahlovi <mdpahlovi07@gmail.com>
2023-07-14 03:45:55 +0600

    [Done] Project Init

---
+++ src/app/routes/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { BookRoutes } from '../modules/books/book.route';
+
+const router = express.Router();
+
+const moduleRoutes = [
+    {
+        path: '/book',
+        route: BookRoutes,
+    },
+];
+
+moduleRoutes.forEach(route => router.use(route.path, route.route));
+export default router;
---
+++ src/errors/handleCastError.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import { IGenericErrorResponse } from '../interfaces/common';
+import { IGenericErrorMessage } from '../interfaces/error';
+
+const handleCastError = (error: mongoose.Error.CastError): IGenericErrorResponse => {
+    const errors: IGenericErrorMessage[] = [
+        {
+            path: error.path,
+            message: 'Invalid Id',
+        },
+    ];
+
+    const statusCode = 400;
+    return {
+        statusCode,
+        message: 'Cast Error',
+        errorMessages: errors,
+    };
+};
+
+export default handleCastError;
